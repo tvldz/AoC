68d2c0e93d21470d6c55716f8a4fc6141e106523
Thomas Valadez <t.valadez@gmail.com>
2024-12-08 21:45:07 -0500

    Create 8.go

---
+++ 24/8/8.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	type location struct {
+		row    int
+		column int
+	}
+
+	var matrix [][]rune
+
+	file, err := os.Open("8.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		runes := []rune(line)
+		matrix = append(matrix, runes)
+	}
+	height := len(matrix[0]) - 1
+	width := len(matrix) - 1
+
+	antennas := make(map[rune][]location)
+	antinodeLocations := make(map[location]int)
+	for rowIndex, row := range matrix {
+		for colIndex, column := range row {
+			if column != '.' {
+				newLocation := location{row: rowIndex, column: colIndex}
+				antennas[column] = append(antennas[column], newLocation)
+			}
+		}
+	}
+	for frequency, locations := range antennas {
+		for location1Index, location1 := range locations {
+			for location2Index, location2 := range locations {
+				if location1Index == location2Index {
+					continue
+				} else {
+					antinodeX := 2*location2.column - location1.column
+					antinodeY := 2*location2.row - location1.row
+					antinode := location{column: antinodeX, row: antinodeY}
+					if antinode.row <= height && antinode.column <= width && antinode.row >= 0 && antinode.column >= 0 {
+						antinodeLocations[antinode] = 1
+					}
+				}
+			}
+		}
+	}
+	fmt.Println(len(antinodeLocations))
+}
